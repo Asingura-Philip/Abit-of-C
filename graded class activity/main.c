2e1240a237627ad397a4f4a53bea9511a2c15ac4
Asingura-Philip <134685900+Asingura-Philip@users.noreply.github.com>
2023-06-19 09:26:39 +0300

    Update main.c

--- graded class activity/main.c
+++ graded class activity/main.c
@@ -6,14 +6,15 @@
 int main() {
 	
 	int salary;
+	printf("Hello Joan ,,, what was this month's salary? ");
+	scanf("%d", &salary);
+	printf("Your salary is %d\n", salary);
+	
 	int rent = 0.05 * salary;
 	int mother_allowance = 0.1 * salary;
 	int monthly_purchases = 380000;
 	int total_expenses = rent + mother_allowance + monthly_purchases;
-	
-	printf("Hello Joan ,,, what was this month's salary? ");
-	scanf("%d", &salary);
-	printf("Your salary is %d\n", salary);
+	printf("Your total expenses are %d\n", total_expenses);
 	
 	if(salary >= total_expenses){
 		printf("you have enough to cater for all expenses");
