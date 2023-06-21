ac4c29347261949664fc3a544dc3f40ea33f6459
Asingura-Philip <134685900+Asingura-Philip@users.noreply.github.com>
2023-06-21 10:11:08 +0300

    for loops in c

---
+++ loops/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+/*	int countdown = 10;
+	for(countdown = 10; countdown > 0; countdown--){
+		printf("countdown: %d\n", countdown);
+	}
+	printf("the rocket has launched!\n");
+	*/
+	
+/*	int number = 5;
+	int i;
+	for(i = 1 ; i <= 10; i++){
+		printf("%d x %d = %d\n", number, i, number* i);
+	}
+	*/
+	//loop that counts number of students in this class
+	
+	
+	int numberofstudents = 22;
+	for(numberofstudents = 1; numberofstudents <= 22; numberofstudents++){
+		printf("student present: %d\n ",numberofstudents);
+	}
+	
+	return 0;	
+	
+	
+}                                                					                                                                        
